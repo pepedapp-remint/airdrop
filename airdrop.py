eb34379975e63102b0ff24968d327f990e489924
chef-pepe <ap334@disroot.org>
2020-10-10 21:33:52 -0700

    Add PepCore ABI + start writing script for tx interpretation

---
+++ airdrop.py
@@ -0,0 +1,5 @@
+# decode function from tx input using contract!
+# 1. set up w3 w/infura
+# 2. load contract in with ABI found in PepeCore.json
+# 3. try decoding a function call from transactions.csv
+# 4. simulate txes in order for drop!
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='pepe-airdrop',
+    version='0.1.0',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "web3>=5.12.1"
+    ]
+)

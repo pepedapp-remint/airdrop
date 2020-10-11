eda8a091ebff9884fdfe99fea29e18a5991968d6
chef-pepe <ap334@disroot.org>
2020-10-11 13:55:24 -0700

    Properly get ownership map!

--- airdrop.py
+++ airdrop.py
@@ -1,3 +1,4 @@
+import copy
 import csv
 import json
 from typing import NamedTuple, Dict, List
@@ -34,6 +35,26 @@ Address = str
 Sig = str
 
 
+class Transfer(NamedTuple):
+    sig: Sig
+    from_addr: Address
+    to_addr: Address
+    count: int
+
+
+def load_transfers():
+    with open('data/events.json', 'r') as f:
+        logs_data = json.load(f)
+
+    transfers = []
+    for log in logs_data:
+        sig = log['topics'][1]
+        from_addr = '0x' + log['topics'][2][26:]
+        to_addr = '0x' + log['topics'][3][26:]
+        count = int(log['data'], 0)
+        transfers.append(Transfer(sig, from_addr, to_addr, count))
+
+
 class Sale(NamedTuple):
     seller: Address
     price: int
@@ -41,7 +62,6 @@ class Sale(NamedTuple):
 
 class PepeState(NamedTuple):
     ownership: Dict[Address, Dict[Sig, int]]
-    sales: Dict[Sig, List[Sale]]
 
 
 def get_all_sigs():
@@ -53,9 +73,30 @@ def get_sig_count(sig: Sig):
 
 
 def load_initial_state():
-    # sigs =
-    ...
+    sigs = get_all_sigs()
+    ownership = {ADMIN_ADDRESS: {}}
+    for sig in sigs:
+        count = get_sig_count(sig)
+        ownership[ADMIN_ADDRESS]['0x' + sig.hex()] = count
+    return PepeState(ownership)
 
+
+def process_transfer(state: PepeState, transfer: Transfer) -> PepeState:
+    if state.ownership[transfer.from_addr][transfer.sig] < transfer.count:
+        raise Exception(f"failed to process transfer {transfer}")
+
+    ret = PepeState(copy.deepcopy(state.ownership))
+    ret.ownership[transfer.from_addr][transfer.sig] -= transfer.count
+
+    if transfer.to_addr not in ret.ownership:
+        ret.ownership[transfer.to_addr] = {}
+
+    if transfer.sig not in ret.ownership[transfer.to_addr]:
+        ret.ownership[transfer.to_addr][transfer.sig] = 0
+
+    ret.ownership[transfer.to_addr][transfer.sig] += transfer.count
+
+    return ret
 
 # State =
 # sales: sig -> list of (price, seller)

1164fcbc667e7b68d445c63cb49f81f7e338f33b
chef-pepe <ap334@disroot.org>
2020-10-11 17:39:03 -0700

    Check tree and prepare json

--- merkle.py
+++ merkle.py
@@ -11,7 +11,13 @@ def hash_leaf(t) -> bytes:
 
 
 def combine_nodes(n1: bytes, n2: bytes) -> bytes:
-    hb = Web3.solidityKeccak(['bytes32', 'bytes32'], [n1, n2])
+    # NOTE: sorts s.t. the smaller byte array always first.
+    # Meant to be identical to https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/cryptography/MerkleProof.sol
+    if n1 <= n2:
+        hb = Web3.solidityKeccak(['bytes32', 'bytes32'], [n1, n2])
+    else:
+        hb = Web3.solidityKeccak(['bytes32', 'bytes32'], [n2, n1])
+
     return bytes(hb)
 
 
@@ -32,6 +38,19 @@ def build_index_tuples():
 class MerkleTree(NamedTuple):
     root: bytes
     leaf_to_branch: Dict[bytes, List[bytes]]
+
+    def jsonify(self):
+        hex_root = self.root.hex()
+
+        hex_ltb = {}
+        for leaf, branch in self.leaf_to_branch.items():
+            hex_branch = [b.hex() for b in branch]
+            hex_ltb[leaf.hex()] = hex_branch
+
+        return {
+            'root': hex_root,
+            'leaf_to_branch': hex_ltb
+        }
 
 
 NULL_NODE = b'\x00' * 32
@@ -63,7 +82,6 @@ def build_tree(index_tuples) -> MerkleTree:
                 leaf_to_branch[leaf].append(n1)
 
             p = combine_nodes(n1, n2)
-
             cur_layer_node_to_leaves[p].extend(n1_leaves + n2_leaves)
             next_layer.append(p)
 
@@ -72,3 +90,11 @@ def build_tree(index_tuples) -> MerkleTree:
     assert len(cur_layer) == 1
 
     return MerkleTree(cur_layer[0], dict(leaf_to_branch))
+
+
+def check_branch(root: bytes, leaf: bytes, branch: List[bytes]) -> bool:
+    cur_node = leaf
+    for pair_node in branch:
+        cur_node = combine_nodes(cur_node, pair_node)
+
+    return cur_node == root

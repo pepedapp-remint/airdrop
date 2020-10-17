88f444b4cdee6af381cc9c41d051e8a8f4bfb00d
chef-pepe <ap334@disroot.org>
2020-10-17 16:56:18 -0700

    Move airdrop up

--- sql/events.sql
+++ sql/events.sql
@@ -1,4 +1,4 @@
 -- Select all `CardsTransferred` event logs
 SELECT * FROM `bigquery-public-data.crypto_ethereum.logs`
-WHERE ARRAY_LENGTH(topics) > 0 AND topics[OFFSET(0)] = '0xca854e9a512debe5ebf4a5228b275f9d8c3c005f95ba3660d7a6a66fdc72b112' AND block_number < 10980000
+WHERE ARRAY_LENGTH(topics) > 0 AND topics[OFFSET(0)] = '0xca854e9a512debe5ebf4a5228b275f9d8c3c005f95ba3660d7a6a66fdc72b112' AND block_number < 11075000
 ORDER by block_number, transaction_index, log_index
